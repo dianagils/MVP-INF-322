6eb7755ba7a20480e2fab0df02c07d720c28f6dd
Vania <gallardo.vania8@gmail.com>
2022-11-29 10:55:31 -0300

    commit

---
+++ src/components/team-card3.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import { Button } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+
+
+export default function TeamCard3({title, image, description}) {
+  const theme = createTheme({
+    typography:  {
+      fontFamily: ["Anek Devanagari", "sans-serif"].join(","),
+      fontSize: 20,
+      fontWeight: 500
+    }
+  });
+  return (
+    <ThemeProvider theme={theme}>
+    <Card sx={{ width: 310 }}>
+      <CardHeader
+        align="center"
+        title={title}
+        subheader="Campus SJ"
+        fontSize="20px"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={image}
+        alt="PreuAPP"
+      />
+      <CardContent>
+        <Typography variant="body" color="text.bold" fontWeight="bolder">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Button  variant='contained' aria-label="Visitar" href="/stand3">
+          VISITAR
+        </Button>
+      </CardActions>
+    </Card>
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/home3.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import NavBar from '../components/nav_bar';
+import { useParams, userLocalStore} from "react-router-dom";
+
+import ResponsiveAppBar from '../components/responsive_app_bar';
+import TeamCard3 from '../components/team-card3';
+import Stack from '@mui/material/Stack';
+import PreuAPP from '../assets/preuapp.jpg';
+import moreLife from '../assets/+Life.jpg';
+import activate from '../assets/activate.jpg';
+import AII from '../assets/AII.jpg';
+import AITrainer from '../assets/aitreiner.jpg';
+import AT from '../assets/AT.jpg';
+import Binmo from '../assets/binmo.jpg';
+import DACOT from '../assets/dacot.jpg';
+import EMPATY from '../assets/empaty.jpg';
+import PUDUPAL from '../assets/pudupal.jpg';
+import VINCULADOS from '../assets/vinculados.jpg';
+import PersistentDrawerRight from '../components/drawer';
+
+export const HomePage3 = () => {
+  const { id } = useParams();
+  return (
+    <div className='navbar'>
+    <ResponsiveAppBar id={id}/>
+    <div className='page'>
+    <PersistentDrawerRight/>
+      <h1 className='page__title'>Bienvenido a la Feria de Software Virtual 2023</h1>
+      <h2>Revisa los proyectos de esta edición y visita sus stands virtuales</h2>
+    </div>
+    <h1 className='project__title'>Categoría Inteligencia Artifical y Ciencia de Datos</h1>
+    <div className='team'>
+      <div className='row'>
+    <Stack spacing={2}  direction='row'  alignItems="center">
+      <TeamCard3 title = "MoreLife" image = {moreLife} description="Sistema informático para la predicción de escasez sanguínea."/>
+      <TeamCard3 title = "Activate" image = {activate} description="Activate simula ser un gimnasio para tu cerebro. "/>
+      <TeamCard3 title = "Artifical Inventory" image = {AII} description="Buscamos evitar quiebres de stock haciendo uso de IA"/>
+      <TeamCard3 title = "TrAIner" image = {AITrainer} description="Asistente virtual con IA para motivar el ejercicio" />
+    </Stack>
+    </div>
+    <div className='row'>
+    <Stack spacing={2} direction='row'  alignItems="center">
+      <TeamCard3 title = "AllegroTraining" image = {AT} description="Aplicación pensada para practicantes de guitarra y violín"/>
+      <TeamCard3 title = "BINMO" image = {Binmo} description="Sistema de detección de bullying mediante análisis de video"/>
+    </Stack>
+    </div>
+    </div>
+    <h1 className='project__title'>Categoría Transformación Digital y Social</h1> <div className='team'>
+    <div className='row'>
+    <Stack spacing={2} direction='row'  alignItems="center">
+      <TeamCard3 title = "Empaty" image = {EMPATY} description="Aplicación para psicólogos que permite realizar terapia online." />
+      <TeamCard3 title = "Vinculados" image = {VINCULADOS} description="Permite conectar las necesidades sociales con las instituciones."/>
+      <TeamCard3 title = "PuduPal" image = {PUDUPAL} description="  Asistente interactivo para web orientado a adultos mayores."/>
+      <TeamCard3 title = "PreuAPP" image = {PreuAPP} description="Preparando la PAES con desafíos de corta duración y gamificación." />    </Stack>
+      </div>
+      <div className='row'>
+      <TeamCard3 title = "Dacot" image = {DACOT} description="Plataforma web enfocada en la transformación digital de la CoT"/>
+
+    </div>
+      </div>  
+    </div>
+  )
+}
+
+export default HomePage3
