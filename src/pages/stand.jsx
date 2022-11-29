614c370318ac91018e82fba82cd89594ebdc03f5
Mariogros <52615558+Mariogros@users.noreply.github.com>
2022-11-29 01:47:07 -0300

    agregado los integrantes

--- src/pages/stand.jsx
+++ src/pages/stand.jsx
@@ -3,20 +3,25 @@ import { useParams, userLocalStore} from "react-router-dom";
 import LogoUSM from '../assets/usm.png'
 import Binmo from '../assets/binmo.jpg';
 import './estilos.css';
-
 import ResponsiveAppBar from '../components/responsive_app_bar';
-
-import Stack from '@mui/material/Stack';
-import { Button } from '@mui/material';
-
-
-
-import ResponsiveAppBar from '../components/responsive_app_bar';
+import TeamCard from '../components/team-card';
+import IntegrantesCard from '../components/integrantes-card';
 import Stack from '@mui/material/Stack';
 import { Button } from '@mui/material';
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 import PersistentDrawerRight from '../components/drawer';
+import PreuAPP from '../assets/preuapp.jpg';
+import moreLife from '../assets/+Life.jpg';
+import activate from '../assets/activate.jpg';
+import AII from '../assets/AII.jpg';
+import AITrainer from '../assets/aitreiner.jpg';
+import AT from '../assets/AT.jpg';
+import DACOT from '../assets/dacot.jpg';
+import EMPATY from '../assets/empaty.jpg';
+import PUDUPAL from '../assets/pudupal.jpg';
+import VINCULADOS from '../assets/vinculados.jpg';
+import PERFIL from '../assets/perfil.png';
 
 export const Stands = () => {
 
@@ -51,11 +56,22 @@ export const Stands = () => {
                 </div>
         </Stack>
         </div>
-        <div className="my-text">
-        <p style={{fontSize: '18px', fontWeight: 'bold', color: 'blue'}}>Hola Mundo!</p>
+        <h1 className='page__title' style={{textAlign: "center"}}>Conoce a nuestros integrantes</h1>
+        <div className='team'>
+          <div className='row'>
+        <Stack spacing={2}  direction='row'  alignItems="center">
+          <IntegrantesCard title = "Integrante 1" image = {PERFIL} subheader="Scrum Master"/>
+          <IntegrantesCard title = "Integrante 2" image = {PERFIL} subheader="Backend Developer"/>
+          <IntegrantesCard title = "Integrante 3" image = {PERFIL} subheader="Frontend Developer"/>
+        </Stack>
         </div>
-    
-
+        <div className='row'>
+        <Stack spacing={2} direction='row'  alignItems="center">
+          <IntegrantesCard title = "Integrante 4" image = {PERFIL} subheader="Product Owner"/>
+          <IntegrantesCard title = "Integrante 5" image = {PERFIL} subheader="IA developer"/>
+        </Stack>
+        </div>
+        </div>
     </div>
   )
 }
