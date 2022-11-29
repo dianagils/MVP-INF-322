eb187196bbecfafe73ce17c0e0efa70a4e3110f5
vDruh8 <104955013+vDruh8@users.noreply.github.com>
2022-11-29 04:05:12 -0300

    Update app_router.jsx

--- src/components/app_router.jsx
+++ src/components/app_router.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home'
+import HomePage3 from '../pages/home3'
 import InternalPage from '../pages/internal'
 import LogIn from '../pages/login';
 import Stands from '../pages/stand';
+import Stands3 from '../pages/stand3';
 import StandEdit from '../pages/stand-edit';
 
 const AppRouter = () => {
@@ -12,9 +14,11 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/:id' element={<HomePage />} />
+        <Route path='/profesor' element={<HomePage3 />} />
         <Route path='/internal' element={<InternalPage />} />
         <Route path='/login' element={<LogIn />} />
         <Route path='/stand' element={<Stands />} />
+        <Route path='/stand3' element={<Stands3 />} />
         <Route path='/stand-edit' element={<StandEdit />} />
       </Routes>
     </BrowserRouter>
